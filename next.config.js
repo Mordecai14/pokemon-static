409a947931318765599dbb8185c0aa70f2abb2bc
Aramen Meza <arammendoza.97@gmail.com>
2023-03-08 17:37:21 -0600

    Adding pwa library

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+const withPwa = require("next-pwa")({
+  dest: "public",
+});
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -6,4 +10,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPwa;
